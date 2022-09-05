ebcc91b25e363a31181c657126847a75a938e298
sum-it-up-9 <66559261+sum-it-up-9@users.noreply.github.com>
2022-09-05 23:24:22 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -9,8 +9,8 @@ let transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: 'samv9668@gmail.com',
-        pass: 'eqguytwzoncvojrf'
+        user: 'om',
+        pass: 'rf'
     }
 });
 
